0f4547a9a6616877c73f362918ea9d71623379cf
Yarit <yarit.villalobos@mail.escuelaing.edu.co>
2021-10-06 08:25:54 -0500

    video profe

---
+++ src/main/java/edu/eci/seguridad/App.java
@@ -0,0 +1,34 @@
+package edu.eci.seguridad;
+
+import spark.Filter;
+import static spark.Spark.*;
+
+
+
+
+public class App {
+
+	
+    public static void main(String [] args){;
+    //secure("keystores/ecikeystore.p12", "123456", null, null);
+        port(getPort());
+      //  after((Filter) (request, response) -> {
+    	//	response.header("Access-Control-Allow-Origin","*");
+    		//response.header("Access-Control-Allow-Methods","GET");
+    	//});
+        //sc= initSecureContext();
+        get("hello", (req,res) -> "Hello Dockerrr!");
+        //get("login",(req,res) -> sc.valida);
+  }
+
+
+
+private static int getPort() {
+      if (System.getenv("PORT") != null) {
+          return Integer.parseInt(System.getenv("PORT"));
+      }
+      return 5001;
+  }
+  
+  
+}
